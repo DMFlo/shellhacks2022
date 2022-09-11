d09306665d962be2b820fd54bb6b908bbdadbfec
Henry Graves <henrygraves11@yahoo.com>
2022-09-10 22:09:12 -0400

    sign in connected to db

--- frontend/src/pages/LogIn.js
+++ frontend/src/pages/LogIn.js
@@ -5,6 +5,15 @@ import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/CardContent';
 import CardContent from '@mui/material/CardContent';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { auth, provider, db } from "../firebase-config";
+import { doc, setDoc, getDoc } from "firebase/firestore";
+import {
+    Routes,
+    Route,
+    Navigate
+  } from "react-router-dom"; 
+import Home from "./Home";
 
 const cardStyle = {
     backgroundColor: "transparent",
@@ -31,6 +40,36 @@ const buttonStyle = {
     borderColor: "white"
 };
 
+// use this instead of AuthStateListener, which I couldn't get to detect auth changes.
+function signInWithGoogle() {
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+    .then((result) => {
+        console.log("auth state changed in LogIn")
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        console.log(user)
+        // if not in database already, make entry
+        setDoc(doc(db, "users", user.uid), {
+            name: "Los Angeles",
+            state: "CA",
+            country: "USA"
+        }).then(
+            (response) => console.log(response)
+        ).catch(
+            (error) => console.log(error)
+        );
+        // redirect after login
+        window.location.href = 'http://localhost:3000/Home'; // todo comment out for prod
+        // window.location.href = 'https://ucf-shellhacks.web.app/Home'; //todo uncomment for prod, improve security (by enforcing auth for /Home?)
+    }).catch((error) => {
+        // Handle Errors here.
+    });
+}
+
 const Login = () => {
     return (
         <div style={{width: "100%", height: "100vh", display: "grid", placeItems: "center", backgroundImage:"linear-gradient(120deg,#004977, #d03027)"}}>
@@ -38,7 +77,7 @@ const Login = () => {
                 <CardContent style={{ width: "100%", height: "100%", display:"grid"}}>
                 <img src={logo} alt='logo' style={logoStyle}/>
                 <div>
-                    <Button type="primary" variant="outlined" size="medium" style={buttonStyle} block>Sign In</Button> 
+                    <Button onClick={signInWithGoogle} type="primary" variant="outlined" size="medium" style={buttonStyle} block>Sign In</Button> 
                 </div>
                 </CardContent>
             </Card>
