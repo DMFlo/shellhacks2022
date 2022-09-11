bf6bfff248bb247c899517ded42f2a16f068a234
cjherrera-ucf <70386738+cjherrera1@users.noreply.github.com>
2022-09-10 22:07:58 -0400

    Update App.js
    cruddy beginnings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,27 @@ import Login from './pages/LogIn';
 import Home from './pages/Home';
 import './App.css';
 import { db, auth } from "./firebase-config";
-
+import { collection, getDocs } from "firebase/firestore";
 import {
   Routes,
   Route,
 } from "react-router-dom";
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  const usersCollectRef = collection(db, "users");
+  
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectRef);
+      setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+    };
+
+    getUsers();
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Login/>} />
